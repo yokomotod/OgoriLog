85bfc2edd668001107ee34a5192cfa5810567004
yokomotod <yokomotod@gmail.com>
2015-02-24 15:45:39 +0900

    Add pod BlocksKit

--- Podfile
+++ Podfile
@@ -2,6 +2,7 @@ source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '8.0'
 
 target 'OgoriLog' do
+  pod 'BlocksKit'
   pod 'Snap'
 end
 
--- Pods/Target Support Files/Pods-OgoriLog/Pods-OgoriLog-frameworks.sh
+++ Pods/Target Support Files/Pods-OgoriLog/Pods-OgoriLog-frameworks.sh
@@ -48,8 +48,10 @@ code_sign() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework 'BlocksKit.framework'
   install_framework 'Snap.framework'
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework 'BlocksKit.framework'
   install_framework 'Snap.framework'
 fi
